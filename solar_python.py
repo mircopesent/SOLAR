121d8259716db71c6e4289ec39334c0240d10aa9
mircopesent <mirco.pesentibucella@gmail.com>
2023-09-27 15:31:51 +0200

    even better, now it works

--- solar_python.py
+++ solar_python.py
@@ -12,14 +12,14 @@ months=[31,28,31,30,31,30,31,31,30,31,30,31]
 for i in range(1,13):
 
     for j in range(1,months[i-1]+1):
-            file_name='C:/Users/mirco/Documents/EPFL/MA3/solar energy conversion/exercices/GHI-2014 Switzerland/MSG_SIS_D_14/msg.SIS.D_ch02.lonlat_2014'+str(i).zfill(2)+str(j).zfill(2) +'000000.nc'
-            file_name_clipped='C:/Users/mirco/Documents/EPFL/MA3/solar energy conversion/exercices/GHI_clipped/msg.SIS.D_ch02.lonlat_2014'+str(i).zfill(2)+str(j).zfill(2) +'000000_clipped.tif'
-            file_name_vector_layer='C:/Users/mirco/Documents/EPFL/MA3/solar energy conversion/exercices/GHI_vector_layer/msg.SIS.D_ch02.lonlat_2014'+str(i).zfill(2)+str(j).zfill(2) +'000000_vector_layer.tif'
-            file_name_vector_final='C:/Users/mirco/Documents/EPFL/MA3/solar energy conversion/exercices/GHI_vector_final/msg.SIS.D_ch02.lonlat_2014'+str(i).zfill(2)+str(j).zfill(2) +'000000_vector_final.gpkg'
-            file_name_vector_area='C:/Users/mirco/Documents/EPFL/MA3/solar energy conversion/exercices/GHI_vector_area/msg.SIS.D_ch02.lonlat_2014'+str(i).zfill(2)+str(j).zfill(2) +'000000_vector_area.gpkg'
-            file_name_output='C:/Users/mirco/Documents/EPFL/MA3/solar energy conversion/exercices/data/data'+str(i).zfill(2)+str(j).zfill(2)+'.csv'
-            file_name_html='C:/Users/mirco/Documents/EPFL/MA3/solar energy conversion/exercices/max_values/stats_2014'+str(i).zfill(2)+str(j).zfill(2) +'000000_.html'
-            processing.run("gdal:cliprasterbymasklayer", {'INPUT':file_name,'MASK':'C:/Users/mirco/Documents/EPFL/MA3/solar energy conversion/exercices/Swiss_boundaries/swissBOUNDARIES3D_1_3_LV95_LN02.gdb|layername=TLM_LANDESGEBIET','SOURCE_CRS':None,'TARGET_CRS':None,'TARGET_EXTENT':None,'NODATA':None,'ALPHA_BAND':False,'CROP_TO_CUTLINE':True,'KEEP_RESOLUTION':False,'SET_RESOLUTION':False,'X_RESOLUTION':None,'Y_RESOLUTION':None,'MULTITHREADING':False,'OPTIONS':'','DATA_TYPE':0,'EXTRA':'','OUTPUT':file_name_clipped})
+            file_name='C:/Users/mirco/Documents/EPFL/MA3/solar_energy_conversion/exercices/GHI-2014 Switzerland/MSG_SIS_D_14/msg.SIS.D_ch02.lonlat_2014'+str(i).zfill(2)+str(j).zfill(2) +'000000.nc'
+            file_name_clipped='C:/Users/mirco/Documents/EPFL/MA3/solar_energy_conversion/exercices/GHI_clipped/msg.SIS.D_ch02.lonlat_2014'+str(i).zfill(2)+str(j).zfill(2) +'000000_clipped.tif'
+            file_name_vector_layer='C:/Users/mirco/Documents/EPFL/MA3/solar_energy_conversion/exercices/GHI_vector_layer/msg.SIS.D_ch02.lonlat_2014'+str(i).zfill(2)+str(j).zfill(2) +'000000_vector_layer.tif'
+            file_name_vector_final='C:/Users/mirco/Documents/EPFL/MA3/solar_energy_conversion/exercices/GHI_vector_final/msg.SIS.D_ch02.lonlat_2014'+str(i).zfill(2)+str(j).zfill(2) +'000000_vector_final.gpkg'
+            file_name_vector_area='C:/Users/mirco/Documents/EPFL/MA3/solar_energy_conversion/exercices/GHI_vector_area/msg.SIS.D_ch02.lonlat_2014'+str(i).zfill(2)+str(j).zfill(2) +'000000_vector_area.gpkg'
+            file_name_output='C:/Users/mirco/Documents/EPFL/MA3/solar_energy_conversion/exercices/data/data'+str(i).zfill(2)+str(j).zfill(2)+'.csv'
+            file_name_html='C:/Users/mirco/Documents/EPFL/MA3/solar_energy_conversion/exercices/max_values/stats_2014'+str(i).zfill(2)+str(j).zfill(2) +'000000_.html'
+            processing.run("gdal:cliprasterbymasklayer", {'INPUT':file_name,'MASK':'C:/Users/mirco/Documents/EPFL/MA3/solar_energy_conversion/exercices/Swiss_boundaries/swissBOUNDARIES3D_1_3_LV95_LN02.gdb|layername=TLM_LANDESGEBIET','SOURCE_CRS':None,'TARGET_CRS':None,'TARGET_EXTENT':None,'NODATA':None,'ALPHA_BAND':False,'CROP_TO_CUTLINE':True,'KEEP_RESOLUTION':False,'SET_RESOLUTION':False,'X_RESOLUTION':None,'Y_RESOLUTION':None,'MULTITHREADING':False,'OPTIONS':'','DATA_TYPE':0,'EXTRA':'','OUTPUT':file_name_clipped})
             processing.run("native:rasterlayerstatistics", {'INPUT':file_name_clipped,'BAND':1,'OUTPUT_HTML_FILE':file_name_html})
 
             HTMLFileToBeOpened = open(file_name_html, "r")
